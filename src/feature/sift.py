f1907f4b52d29412ed31f16e8ea3ae21098ae929
15cm <zhuhaitong15@gmail.com>
2015-11-22 17:40:29 +0800

    superpixel implemented

--- src/feature/sift.py
+++ src/feature/sift.py
@@ -4,3 +4,15 @@ Created by 15 cm on 11/22/15 4:13 PM
 Copyright © 2015 15cm. All rights reserved.
 '''
 __author__ = '15cm'
+
+import cv2
+
+
+class Sift:
+
+    def __init__(self,edgeThreshold=0.2,sigma=3):
+        self.sift = cv2.SIFT(edgeThreshold=edgeThreshold,sigma=sigma)
+
+    def compute(self,image):
+        gray_im = cv2.cvtColor(cv2.imread(image),cv2.COLOR_BGR2GRAY)
+        return self.sift.Compute(gray_im,None)
--- src/feature/superpixel.py
+++ src/feature/superpixel.py
@@ -4,3 +4,29 @@ Created by 15 cm on 11/22/15 4:14 PM
 Copyright © 2015 15cm. All rights reserved.
 '''
 __author__ = '15cm'
+
+import os
+import cv2
+import numpy as np
+from skimage.segmentation import slic
+from skimage.util import img_as_float
+
+CURPATH = os.path.split(os.path.realpath(__file__))[0]
+DATAPATH = os.path.join(os.path.dirname(CURPATH),'dataset')
+
+class SuperPixel:
+
+
+    def __init__(self,image):
+        self.img = cv2.imread(os.path.join(DATAPATH,image))
+
+    def segment(self):
+        self.segments = slic(img_as_float(self.img), enforce_connectivity=True)
+        self.mask = np.zeros(self.img.shape[:2],dtype='int' )
+        self.mask = self.mask - 1
+        for (i, segVal) in enumerate(np.unique(self.segments)):
+            self.mask[segVal == self.segments] = i
+
+
+
+
